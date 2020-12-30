b63bf40beda96f89bc35cef6545ea6d9dba43242
geo-lima <72469071+geo-lima@users.noreply.github.com>
2020-12-30 13:50:46 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+
+						<html>
+						<head>
+						<meta charset="utf-8">
+						<title> svendas </title>
+						<link rel="stylesheet" type="text/css" href="layout.css">
+                       <script src="jquery-3.5.1.min.js"></script>
+                       <script src="scripts.js"></script>
+						<body>
+							<div class="topo"> 
+					         <div class="logo">         	
+                                 <img src="logo.png" width="230" height="230">
+                                 
+					         </div>      
+					         <div class="menu">
+                             <ul>
+
+<li><a href="index.php?p=vc">Cadastrar equipamento</a></li>
+							 
+<li><a href="index.php?p=cc">Cadastrar cliente</a></li>
+
+<li><a href="index.php?p=cf">Consultar estoque</a></li>
+
+<li><a href="index.php?p=rv">Realizar venda</a></li>
+
+</ul>
+
+ </div>
+					         
+</div>
+<div class="fundo"> 
+	                         <div class="imgfundo">            
+                                 <img src="RET.png" width="80" height="80">                      
+                            </div>                        
+	                         <div class="imgfundo1">
+                                 <img src="RET.png" width="80" height="80" >                                                           
+                         </div>							
+                                         
+			
+
+						</div> 
+						</body>
+						</head>
+                        </html>
+                      
---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+
+						<html>
+						<head>
+						<meta charset="utf-8">
+						<title> svendas </title>
+						<link rel="stylesheet" type="text/css" href="layout.css">
+                       <script src="jquery-3.5.1.min.js"></script>
+                       <script src="scripts.js"></script>
+						<body>
+							<div class="topo"> 
+					         <div class="logo">         	
+                                 <img src="logo.png" width="230" height="230">
+                                 
+					         </div>      
+					         <div class="menu">
+                             <ul>
+
+<li><a href="index.php?p=vc">Cadastrar equipamento</a></li>
+							 
+<li><a href="index.php?p=cc">Cadastrar cliente</a></li>
+
+<li><a href="index.php?p=cf">Consultar estoque</a></li>
+
+<li><a href="index.php?p=rv">Realizar venda</a></li>
+
+
+
+</ul>
+
+ </div>
+					         
+</div>
+<div class="fundo"> 
+	                         <div class="imgfundo">            
+                                 <img src="RET.png" width="80" height="80">                      
+                            </div>                        
+	                         <div class="imgfundo1">
+                                 <img src="RET.png" width="80" height="80" >                                                           
+                         </div>							
+                                         
+<div id="conteudo" class="conteudo">
+<?php
+
+if(isset($_GET['p'])){
+
+    $pagina = $_GET['p'];
+
+}else{
+
+    $pagina = 'home.php';
+
+}
+
+if($pagina == 'cc'){
+
+    include "cliente_form.html";
+
+}elseif($pagina == 'vc'){
+
+    include "equipamento_form.html";
+
+}elseif($pagina == 'cf'){
+
+    include "equipamento_ver.php";
+
+}elseif($pagina == 'rv'){
+
+    include "venda_form.html";
+
+}
+
+?>				
+
+						</div> 
+						</body>
+						</head>
+                        </html>
+                      
